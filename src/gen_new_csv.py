5da387e3e241cac60011c74696b1491f0a632adf
Farrell Meng <mengfanqi0828@163.com>
2019-09-20 03:03:21 +0800

    Get rid of useless cell index and add RSRP_Pool col.

--- src/gen_new_csv.py
+++ src/gen_new_csv.py
@@ -12,11 +12,17 @@ test_dfs = [pd.read_csv(test_csv) for test_csv in test_csvs]
 test_df = pd.concat(test_dfs)
 test_df = test_df.reset_index(drop=True)
 
-test_cell_clutter_dummy = pd.get_dummies(pd.Categorical(test_df['Cell Clutter Index'], categories=range(1, 21)), prefix='CellClutterIndex_')
-train_cell_clutter_dummy = pd.get_dummies(pd.Categorical(train_df['Cell Clutter Index'], categories=range(1, 21)), prefix='CellClutterIndex_')
+unique_clutter_index = list(set(list(train_df['Cell Clutter Index'].unique()) 
+                              + list(train_df['Clutter Index'].unique()) 
+                              + list(test_df['Cell Clutter Index'].unique()) 
+                              + list(test_df['Clutter Index'].unique())))
+unique_clutter_index.sort()
 
-test_clutter_dummy = pd.get_dummies(pd.Categorical(test_df['Clutter Index'], categories=range(1, 21)), prefix='ClutterIndex_')
-train_clutter_dummy = pd.get_dummies(pd.Categorical(train_df['Clutter Index'], categories=range(1, 21)), prefix='ClutterIndex_')
+test_cell_clutter_dummy = pd.get_dummies(pd.Categorical(test_df['Cell Clutter Index'], categories=unique_clutter_index), prefix='CellClutterIndex')
+train_cell_clutter_dummy = pd.get_dummies(pd.Categorical(train_df['Cell Clutter Index'], categories=unique_clutter_index), prefix='CellClutterIndex')
+
+test_clutter_dummy = pd.get_dummies(pd.Categorical(test_df['Clutter Index'], categories=unique_clutter_index), prefix='ClutterIndex')
+train_clutter_dummy = pd.get_dummies(pd.Categorical(train_df['Clutter Index'], categories=unique_clutter_index), prefix='ClutterIndex')
 
 test_df = (test_df.merge(test_cell_clutter_dummy, left_index=True, right_index=True)
                   .merge(test_clutter_dummy, left_index=True, right_index=True))
@@ -28,5 +34,7 @@ train_df = (train_df.merge(train_cell_clutter_dummy, left_index=True, right_inde
 del train_df['Cell Clutter Index']
 del train_df['Clutter Index']
 
-train_df.to_csv('./data/train_v1.csv', index=False)
-test_df.to_csv('./data/test_v1.csv', index=False)
+train_df['RSRP_Poor'] = train_df['RSRP'] < -103
+
+train_df.to_csv('./data/train_v2.csv', index=False)
+test_df.to_csv('./data/test_v2.csv', index=False)
