140f7b83f62ca9bbea2691f0c3b3f94a504537eb
Meng, Fanqi <fanqi.meng@intel.com>
2019-09-20 10:56:23 +0800

    Fixed the data type bug.

--- src/losses.py
+++ src/losses.py
@@ -2,6 +2,7 @@ from keras import backend as K
 from keras.losses import mean_squared_error 
 
 def wmse(y_true, y_pred, threshold=-103, weights=5):
-    mask = y_true < -103
+    pos_mask = K.cast(y_true < -103, 'float')
+    neg_mask = K.cast(y_true >= -103, 'float')
     mse = mean_squared_error(y_true, y_pred)
-    return weights * mse * mask + mse * ~mask
+    return weights * mse * pos_mask + mse * neg_mask
