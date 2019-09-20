9ece55861b3dccc31f4e74c8f17d6b5f44c3710b
qqwangxiaow <ywwangxiaow@qq.com>
2019-09-20 20:04:33 +0800

    add

--- lgb.py
+++ lgb.py
@@ -30,26 +30,3 @@ print(loss)
 print("------importance--------")
 print(reg.feature_importances_)
 
-
-#%%
-from xgboost import XGBRegressor
-
-reg = XGBRegressor()
-model=reg.fit(X_train, y_train)
-y_pred = reg.predict(X_test)
-loss = mean_squared_error(y_test, y_pred)
-print("----------loss_xgb-------")
-print(loss)
-print("------importance--------")
-print(model.feature_importances_) 
-
-from xgboost import XGBRegressor
-
-reg = XGBRegressor()
-model=reg.fit(X_train, y_train)
-y_pred = reg.predict(X_test)
-loss = mean_squared_error(y_test, y_pred)
-print("----------loss_xgb-------")
-print(loss)
-print("------importance--------")
-print(model.feature_importances_)
